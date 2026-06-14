b48180660ec145764548c304267ea7fb7bc1bfab
TYC-engine <dema29992@gmail.com>
2026-06-14 12:18:41 +0600

    Update gui.c

--- HeliosOS/kernel/gui/gui.c
+++ HeliosOS/kernel/gui/gui.c
@@ -17,3 +17,34 @@ void gui_add_window(Window* win)
     if(window_count < MAX_WINDOWS)
         windows[window_count++] = win;
 }
+
+void gui_render()
+{
+    Surface desktop = surface_create(1024, 768);
+
+    gdi_clear(&desktop, 0x101010);
+
+    for(int i = 0; i < window_count; i++)
+    {
+        Window* w = windows[i];
+
+        window_draw(w);
+
+        for(int y = 0; y < w->h; y++)
+        {
+            for(int x = 0; x < w->w; x++)
+            {
+                int dx = w->x + x;
+                int dy = w->y + y;
+
+                surface_setpixel(
+                    &desktop,
+                    dx, dy,
+                    w->buffer[y * w->w + x]
+                );
+            }
+        }
+    }
+
+    compositor_present(&desktop);
+}
