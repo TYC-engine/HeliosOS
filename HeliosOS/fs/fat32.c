56785fcb502156beb1e0b511d8bf2a0829ccac6b
TYC-engine <dema29992@gmail.com>
2026-06-14 12:00:27 +0600

    Update fat32.c

--- HeliosOS/fs/fat32.c
+++ HeliosOS/fs/fat32.c
@@ -3,26 +3,28 @@
 
 static FAT32_BPB bpb;
 
-static uint32_t fat_start;
-static uint32_t data_start;
+uint32_t fat_start;
+uint32_t data_start;
 
-int fat32_init(void)
+int fat32_init()
 {
-    if(disk_read_sector(0, &bpb) != 0)
+    if(disk_read_sector(
+        0,
+        &bpb
+    ) != 0)
+    {
         return -1;
+    }
 
-    fat_start = bpb.reserved_sectors;
+    fat_start =
+        bpb.reserved_sectors;
 
     data_start =
         bpb.reserved_sectors +
-        (bpb.fat_count * bpb.fat_size32);
+        (
+            bpb.fat_count *
+            bpb.fat_size32
+        );
 
     return 0;
-}
-
-uint32_t fat32_cluster_to_lba(uint32_t cluster)
-{
-    return data_start +
-        ((cluster - 2) *
-        bpb.sectors_per_cluster);
 }
