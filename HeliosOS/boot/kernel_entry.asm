32e904706aa688635f16e8b26afad544f7788cf5
TYC-engine <dema29992@gmail.com>
2026-06-15 14:15:25 +0600

    Update kernel_entry.asm

--- HeliosOS/boot/kernel_entry.asm
+++ HeliosOS/boot/kernel_entry.asm
@@ -1,11 +1,49 @@
 [BITS 32]
 
 extern kernel_main
+extern gdt_flush
+extern tss_flush
 
 global _start
 
+; =========================
+; STACK
+; =========================
+section .bss
+align 16
+
+stack_bottom:
+    resb 16384
+stack_top:
+
+; =========================
+; ENTRY
+; =========================
+section .text
+align 4
+
 _start:
 
+    ; stack setup
+    mov esp, stack_top
+    mov ebp, esp
+
+    cld
+    cli
+
+    ; -------------------------
+    ; GDT reload (important for TSS)
+    ; -------------------------
+    call gdt_flush
+
+    ; -------------------------
+    ; TSS load
+    ; -------------------------
+    call tss_flush
+
+    ; -------------------------
+    ; jump to kernel C entry
+    ; -------------------------
     call kernel_main
 
 .hang:
